ff3768244b7a355b4f04188c4cd5e60059fd48f4
whowechina <whowechina@gmail.com>
2023-10-22 10:48:24 +0800

    SN display in CLI

--- firmware/src/cli.c
+++ firmware/src/cli.c
@@ -7,6 +7,7 @@
 #include "pico/stdio.h"
 #include "pico/stdlib.h"
 #include "cli.h"
+#include "save.h"
 
 #define MAX_COMMANDS 32
 #define MAX_PARAMETERS 6
@@ -57,6 +58,7 @@ int cli_match_prefix(const char *str[], int num, const char *prefix)
 static void handle_help(int argc, char *argv[])
 {
     printf("%s", cli_logo);
+    printf("\tSN: %016llx\n\n", board_id_64());
     printf("Available commands:\n");
     for (int i = 0; i < num_commands; i++) {
         printf("%*s: %s\n", max_cmd_len + 2, commands[i], helps[i]);
