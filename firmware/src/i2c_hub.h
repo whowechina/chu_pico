7f991e3d5f5935207e761c99c75d2e42201ba9a3
whowechina <whowechina@gmail.com>
2023-12-10 12:20:10 +0800

    Better timing, I think

--- firmware/src/i2c_hub.h
+++ firmware/src/i2c_hub.h
@@ -17,11 +17,18 @@ static inline void i2c_hub_init()
     gpio_init(I2C_HUB_EN);
     gpio_set_dir(I2C_HUB_EN, GPIO_OUT);
     gpio_put(I2C_HUB_EN, 1);
+
+    // reset at start to avoid failed power-on
+    sleep_ms(10);
+    gpio_put(I2C_HUB_EN, 0);
+    sleep_us(50);
+    gpio_put(I2C_HUB_EN, 1);
 }
 
 static inline void i2c_select(i2c_inst_t *i2c_port, uint8_t chn)
 {
     i2c_write_blocking_until(i2c_port, I2C_HUB_ADDR, &chn, 1, false, time_us_64() + 1000);
+    sleep_us(1);
 }
 
 #endif
